65c5fdccfc0d574f88cb488a8641f0d38000a289
Mirko Kiefer <mail@mirkokiefer.com>
2011-08-09 15:55:04 +0200

    update

--- LCAbstractShape.h
+++ LCAbstractShape.h
@@ -1,10 +1,3 @@
-//
-//  LCAbstractShape.h
-//  Wire
-//
-//  Created by Mirko on 7/11/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
-//
 
 #import "LCShape.h"
 @class LCBezierPath, LCGraphicsContext;
